2d74cb9f6a413937c176997ba0ca1e852b804437
Ali Kartal <ali_kartal91@hotmail.com>
2022-10-19 16:56:34 +0300

    update

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
+
 const app = express();
+
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
 // MIDDLEWARES
 app.use(express.static('public'));
 
-const port = 3333;
-
+// ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'template/index.html'));
+  // res.sendFile(path.resolve(__dirname, 'template/index.html'));
+  res.render("index")
 });
+
+
+
+
+const port = 3333;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
