22f931a6a34f40a49a662e3f99da5227321dd4e6
Ali Kartal <ali_kartal91@hotmail.com>
2022-10-19 15:02:13 +0300

    update

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
+
+// MIDDLEWARES
+app.use(express.static('public'));
 
 const port = 3333;
 
 app.get('/', (req, res) => {
-  res.send('Server is ready2');
+  res.sendFile(path.resolve(__dirname, 'template/index.html'));
 });
 
 app.listen(port, () => {
