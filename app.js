2fff80e8c77dcb803910135c1427cb789096ead1
Ali Kartal <ali_kartal91@hotmail.com>
2022-10-19 17:26:12 +0300

    update

--- app.js
+++ app.js
@@ -12,12 +12,18 @@ app.use(express.static('public'));
 
 // ROUTES
 app.get('/', (req, res) => {
-  // res.sendFile(path.resolve(__dirname, 'template/index.html'));
-  res.render("index")
+  // res.sendFile(path.resolve(__dirname, 'template/index.html')); // template kalsöründeki index.html'i göndermek için kullanıyordum bu kodu ama artık gerek yok Template Engine kullanıyorum: https://www.npmjs.com/package/ejs
+
+  res.render('index');
 });
 
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 
-
+app.get('/add', (req, res) => {
+  res.render('add');
+});
 
 const port = 3333;
 
